8d530cd1d431c50b13b9a46fd2110148d2f2e3b7
Jelte Jansen <github@tjeb.nl>
2016-06-08 12:50:47 +0200

    Add beta option to release script

--- scripts/create_release.py
+++ scripts/create_release.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
 import argparse
+import datetime
 import os
 import shutil
+import sys
 
 # Structure:
 # (board_name, binfile_name)
 IMAGES = [
     ("gl-inet", "openwrt-ar71xx-generic-gl-inet-6416A-v1-squashfs-sysupgrade.bin"),
-    ("gl_ar150", "openwrt-ar71xx-generic-gl_ar150-squashfs-sysupgrade.bin")
+    ("gl-ar150", "openwrt-ar71xx-generic-gl-ar150-squashfs-sysupgrade.bin")
 ]
 
 class ReleaseEnvironmentError(Exception):
@@ -26,8 +28,9 @@ class ReleaseCreator:
             raise ReleaseEnvironmentError("Changelog file does not exist: %s" % self.changelog_filename)
         
         for image in IMAGES:
-            if not os.path.exists("bin/ar71xx/%s" % image[1]):
-                raise ReleaseEnvironmentError("Image file for %s does not exist, please build it" % image[0])
+            image_file_path = "bin/ar71xx/%s" % image[1]
+            if not os.path.exists(image_file_path):
+                raise ReleaseEnvironmentError("Image file for %s does not exist (%s), please build it" % (image[0], image_file_path))
 
     def create_target_tree(self):
         if not os.path.exists(self.target_dir):
@@ -64,12 +67,27 @@ class ReleaseCreator:
         self.create_versions_file()
 
 parser = argparse.ArgumentParser(description="Create ValiBox release file structure")
-parser.add_argument("version", help="Version of the release (e.g. 1.0.2)")
+parser.add_argument("-v", "--version", help="Version of the release (e.g. 1.0.2)")
+parser.add_argument("-b", "--beta", help="Create a beta release version (version number will be beta-<datetime>)", action="store_true")
 parser.add_argument("changelog", help="changelog file to put in the release")
 parser.add_argument("targetdir", help="target directory to place files in")
 
 args = parser.parse_args()
 
-rc = ReleaseCreator(args.version, args.changelog, args.targetdir)
+if args.version is None and not args.beta:
+    print("Need either a version (-v) or beta (-b)")
+    sys.exit(1)
+if args.version is not None and args.beta:
+    print("Can't specify both a version number and beta")
+    sys.exit(1)
+
+if args.version is not None:
+    version_number = args.version
+else:
+    dt = datetime.datetime.now()
+    version_number = "beta-%s" % dt.strftime("%Y%m%d%H%M")
+
+#print(version_number)
+rc = ReleaseCreator(version_number, args.changelog, args.targetdir)
 rc.create_release()
 
