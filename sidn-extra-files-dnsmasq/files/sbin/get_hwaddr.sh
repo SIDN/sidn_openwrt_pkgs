b7ed253d1a488823d8395c043190bcde7b9affc8
Jelte Jansen <github@tjeb.nl>
2016-08-09 10:39:36 +0200

    remove experimental code

--- sidn-extra-files-dnsmasq/files/sbin/get_hwaddr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/sbin/ifconfig br-lan | grep HWaddr | head -1 | sed 's/^.*\(.\).\(.\)\(.\)..$/\1\2\3/' | awk '{print tolower($0)}'
--- sidn-extra-files-dnsmasq/files/sbin/get_ip4addr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/sbin/ifconfig br-lan | grep inet | head -1 | cut -d ' ' -f 12 | cut -d ':' -f 2
