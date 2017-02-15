27c95e20995eed9a37f718df507fde5d0ec03e88
Jelte Jansen <github@tjeb.nl>
2017-02-15 13:40:30 +0100

    start/stop/pid control

--- spin/files/usr/sbin/run_conntrack.sh
+++ spin/files/usr/sbin/run_conntrack.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+
+PID=0
+
+stop_conntrack() {
+  if [ $PID != 0 ]; then
+    kill $PID
+  fi
+  exit
+}
+
+run_conntrack() {
+  conntrack -E -o timestamp --buffer-size 327680 > /tmp/spin_pipe &
+  PID=$!
+  wait $PID
+}
+
+trap stop_conntrack TERM KILL INT 
 while true; do
-    conntrack -E -o timestamp > $1
+  run_conntrack
 done
