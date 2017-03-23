3df6815e10d427bc793e2ab910f370b87656afb8
Jelte Jansen <jelte.jansen@sidn.nl>
2017-03-23 14:06:34 +0100

    fix sha256 check

--- autonta/files/www/autonta/lua/valibox_update.lua
+++ autonta/files/www/autonta/lua/valibox_update.lua
@@ -58,7 +58,8 @@ function vu.fetch_firmware_info(base_url, fetch_options)
 end
 
 function vu.get_sha256_sum(filename)
-  local p,err = mio.subprocess(mio.split_cmd("/usr/bin/sha256sum " .. filename))
+  local cmd, args = mio.split_cmd("/usr/bin/sha256sum " .. filename)
+  local p,err = mio.subprocess(cmd, args, nil, true)
   if p == nil then
     au.debug("[XX] error in sha256sum...")
     return nil, err
