51ac29045261958051809e7d895c6c1b655ad2d4
Elmer Lastdrager <elmer.lastdrager@sidn.nl>
2021-01-20 17:31:24 +0100

    Remove ancient directory call leading to errors

--- autonta/files/www/autonta/run_uhttpd_server.sh
+++ autonta/files/www/autonta/run_uhttpd_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-(cd /www/autonta/lua; uhttpd -f -p 8001 -L ./autonta_uhttpd_wrapper.lua  -l "")
+(cd /www/autonta/; uhttpd -f -p 8001 -L ./autonta_uhttpd_wrapper.lua  -l "")
 rm -f /var/autonta.pid
