5cdf1788f7411e73ce3050ee3ba6ebf746baea53
Jelte Jansen <github@tjeb.nl>
2021-09-09 15:30:28 +0200

    Add menu option and empty template for update check

--- autonta/files/usr/lib/lua/luci/controller/valibox/valibox.lua
+++ autonta/files/usr/lib/lua/luci/controller/valibox/valibox.lua
@@ -1,5 +1,7 @@
 module("luci.controller.valibox.valibox", package.seeall)
 
 function index()
-    entry({"admin", "valibox"}, cbi("valibox/valibox"), _("Valibox"), 80)
+    entry({"admin", "valibox"}, firstchild(), _("Valibox"), 80)
+    entry({"admin", "valibox", "settings" }, cbi("valibox/valibox"), _("Settings"), 1)
+    entry({"admin", "valibox", "update_check" }, cbi("valibox/update_check"), _("Check for updates"), 2)
 end
