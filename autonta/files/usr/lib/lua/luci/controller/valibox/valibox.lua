d1188e7ddd98cdf418f19976a98b00206b1efd27
Jelte Jansen <github@tjeb.nl>
2021-09-20 15:23:19 +0200

    Make reload not resubmit by adding a redirect for the success page

--- autonta/files/usr/lib/lua/luci/controller/valibox/valibox.lua
+++ autonta/files/usr/lib/lua/luci/controller/valibox/valibox.lua
@@ -5,4 +5,5 @@ function index()
     entry({"admin", "valibox", "settings" }, cbi("valibox/valibox"), _("Settings"), 1)
     entry({"admin", "valibox", "update_check" }, template("valibox/update_check"), _("Check for updates"), 2)
     entry({"admin", "valibox", "update_install" }, template("valibox/update_install"), nil, nil)
+    entry({"admin", "valibox", "update_done" }, template("valibox/update_done"), nil, nil)
 end
